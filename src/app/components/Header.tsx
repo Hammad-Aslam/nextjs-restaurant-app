0a17e1a27c8b6dafed05ca5ac736e03fb03bd6c5
Hammad <hammadaslam382@gmail.com>
2023-10-17 19:24:11 +0500

    Add Client and Server Components to different page

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,10 +1,6 @@
-"use client";
-import { useRouter } from "next/navigation";
-import React, { useState } from "react";
+import SearchBar from "./SearchBar";
 
 function Header() {
-  const [location, setLocation] = useState("");
-  const router = useRouter();
   return (
     <div className="h-64 bg-gradient-to-r from-[#0f1f47] to-[#5f6984] p-2">
       <div className="text-center mt-10">
@@ -12,24 +8,7 @@ function Header() {
           Find your table for any occasion
         </h1>
         {/* SEARCH BAR */}
-        <div className="text-left text-lg py-3 m-auto flex justify-center">
-          <input
-            className="rounded  mr-3 p-2 w-[450px]"
-            type="text"
-            placeholder="State, city or town"
-            value={location}
-            onChange={(e) => setLocation(e.target.value)}
-          />
-          <button
-            onClick={() => {
-              if (location === "fruit") return;
-              else router.push("/search");
-            }}
-            className="rounded bg-red-600 px-9 py-2 text-white"
-          >
-            Let's go
-          </button>
-        </div>
+        <SearchBar />
         {/* SEARCH BAR */}
       </div>
     </div>
